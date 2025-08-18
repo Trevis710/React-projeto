c1f3299ac049f8a3f41c38172ddeff5deae2b540
Trevis <lipejulien@hotmail.com>
2025-08-18 17:22:38 -0300

    finalizado

---
+++ src/pages/Form.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+
+function Form(){
+return(
+    <>
+    <Header title="Form Page"/>
+        <div>
+            Cadastre seu cliente
+        </div>
+    </>
+)
+}
+
+export default Form;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-
 import { useState } from "react";
-import Header from "./Header";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
-function App() {
+function Home() {
 
   const [contador, setContador] = useState<number>(0);
 
@@ -16,8 +15,10 @@ function btnClick(){
       <Header title="Hello New World"/>
       <button onClick={btnClick}>Click me</button>
       <p>{contador} cliques!</p>  
+      <Link to="/cadastro">Cadastrar cliente</Link>
     </>
   )
 }
 
-export default App
+export default Home;
+
